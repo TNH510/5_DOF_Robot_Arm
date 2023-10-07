5dd2c7a41b5006a19d3fbd4eef4937f0414aeb16
Hieu Tran Ngoc <hieutranngoc@itrvn.com>
2023-10-07 10:47:48 +0700

    update: Reduce kinematic
    [optional footer]

--- matlab/caculate_kinematic.m
+++ matlab/caculate_kinematic.m
@@ -12,10 +12,7 @@ P0 = A1*A2*A3*A4*A5*P
 %P0 = T(0,0,l1).R(z,deta1).T(0,l4,l2).R(z,deta2).T(0,l5,l3-l6-l7).R(z,deta3).P6
 %P = T1*Rz1*T2*Rz2*T3*Rz3*P6
 
-%P0 =
  
-% l3*cos(t1)*cos(t2) - l6*sin(t1) - l42*sin(t1) - l78*(cos(t4)*(cos(t1)*cos(t2)*sin(t3) - cos(t1)*cos(t3)*sin(t2)) - sin(t4)*(cos(t1)*sin(t2)*sin(t3) + cos(t1)*cos(t2)*cos(t3))) + l5*cos(t1)*cos(t2)*cos(t3) + l5*cos(t1)*sin(t2)*sin(t3)
-% l6*cos(t1) - l78*(cos(t4)*(cos(t2)*sin(t1)*sin(t3) - cos(t3)*sin(t1)*sin(t2)) - sin(t4)*(sin(t1)*sin(t2)*sin(t3) + cos(t2)*cos(t3)*sin(t1))) + l42*cos(t1) + l3*cos(t2)*sin(t1) + l5*cos(t2)*cos(t3)*sin(t1) + l5*sin(t1)*sin(t2)*sin(t3)
-%                                                                               l1 + l3*sin(t2) - l78*(cos(t4)*(cos(t2)*cos(t3) + sin(t2)*sin(t3)) + sin(t4)*(cos(t2)*sin(t3) - cos(t3)*sin(t2))) - l5*cos(t2)*sin(t3) + l5*cos(t3)*sin(t2)
-%                                                                                                                                                                                                                                         1
- 
+Px = cos(t1)*(- l78*sin(t3 - t2 - t4) + l3*cos(t2) + l5*cos(t3 - t2)) - (l6 + l42)*sin(t1)
+Py = sin(t1)*(- l78*sin(t3 - t2 - t4) + l3*cos(t2) + l5*cos(t3 - t2)) + (l6 + l42)*cos(t1)
+Pz = l1 + l3*sin(t2) - l78*cos(t3 - t2 - t4) - l5*sin(t3 - t2)
