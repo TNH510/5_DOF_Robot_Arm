c0c376a47df8c6ed607d4835987dcbb5dd257261
Thach Vu <thach6656@gmail.com>
2023-10-21 12:30:16 +0700

    update: Modified 3 files

--- matlab/Forward_Kinematic_Result.m
+++ matlab/Forward_Kinematic_Result.m
@@ -5,7 +5,7 @@ syms th1 th2 th3 th4 th5
 th1 = 0;
 th2 = 0;
 th3 = 0;
-th4 = 0;
+th4 = -pi/2;
 
 l1 = 240;
 l4 = 200;
@@ -15,9 +15,9 @@ l5 = 500;
 l6 = 50;
 l7 = 100; l8 = 0;
 
-px = l6*sin(th1) + sin(th1)*(l2 - l4) + l3*cos(th1)*cos(th2) + sin(th2 - th3 + th4)*cos(th1)*(l7 + l8) + l5*cos(th1)*cos(th2)*cos(th3) + l5*cos(th1)*sin(th2)*sin(th3)
-py = sin(th2 - th3 + th4)*sin(th1)*(l7 + l8) - cos(th1)*(l2 - l4) - l6*cos(th1) + l3*cos(th2)*sin(th1) + l5*cos(th2)*cos(th3)*sin(th1) + l5*sin(th1)*sin(th2)*sin(th3)
-pz = l1 - l7*cos(th2 - th3 + th4) - l8*cos(th2 - th3 + th4) + l3*sin(th2) + l5*sin(th2 - th3)
+px = cos(th1)*(l3*cos(th2 + th3) + l2*cos(th2) + l5*cos(th2 + th3 + th4))
+py = sin(th1)*(l3*cos(th2 + th3) + l2*cos(th2) + l5*cos(th2 + th3 + th4))
+pz = l1 + l3*sin(th2 + th3) + l2*sin(th2) + l5*sin(th2 + th3 + th4)
 
 % px = l1*cos(th1) + l6*sin(th1) + sin(th1)*(l2 - l4) + l3*cos(th1)*cos(th2) + sin(th2 - th3 + th4)*cos(th1)*(l7 + l8) + l5*cos(th1)*cos(th2)*cos(th3) + l5*cos(th1)*sin(th2)*sin(th3)
 % py = l1*sin(th1) - l6*cos(th1) - cos(th1)*(l2 - l4) + sin(th2 - th3 + th4)*sin(th1)*(l7 + l8) + l3*cos(th2)*sin(th1) + l5*cos(th2)*cos(th3)*sin(th1) + l5*sin(th1)*sin(th2)*sin(th3)
