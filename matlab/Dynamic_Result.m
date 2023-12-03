710e0d45adfb34bf38a845820ec5cc246ab96671
Hieu Tran Ngoc <hieutranngoc@itrvn.com>
2023-12-03 15:33:39 +0700

    update: Complete caculate R

--- matlab/Dynamic_Result.m
+++ matlab/Dynamic_Result.m
@@ -3,7 +3,6 @@
 % Variables
 syms px py pz l1 l2 l3 l4 l5
 syms t1 t2 t3 t4 t5
-syms c1 c2 c3 c4 c5 s1 s2 s3 s4 s5
 
 %% Forward Kinematic Result
 % x_value = cos(t1)*(l3*cos(t2 + t3) + l2*cos(t2) + l5*cos(t2 + t3 + t4));
@@ -12,8 +11,8 @@ syms c1 c2 c3 c4 c5 s1 s2 s3 s4 s5
 
 %% MA TRAN CHI HUONG (Ri)
 syms R1 R2 R3 R4 R5
-R1 = [c1 0 s1; s1 0 -c1; 0 1 0]
-R2 = [c1*c2 -c1*s2 s1; s1*c2 -s1*s2 -c1; s2 c2 0]
-R3 = [c1*c23 -c1*s23 s1; s1*c23 -s1*s23 -c1; s23 c23 0]
-R4 = [-c1*s234 s1 c1*c234; -s1*s234 -c1 s1*c234; c234 0 s234]
-R5 = [(s1*s5 - s234*c1*c5) (s1*c5 + s234*c1*s5) (c234*c1); (-c1*s5 - s234*s1*c5 ) (-c1*c5 + s234*s1*s5) (c234*s1); c234*c5 -c234*s5 s234]
+R1 = [cos(t1) 0 sin(t1); sin(t1) 0 -cos(t1); 0 1 0]
+R2 = [cos(t1)*cos(t2) -cos(t1)*sin(t2) sin(t1); sin(t1)*cos(t2) -sin(t1)*sin(t2) -cos(t1); sin(t2) cos(t2) 0]
+R3 = [cos(t1)*cos(t2 + t3) -cos(t1)*sin(t2 + t3) sin(t1); sin(t1)*cos(t2 + t3) -sin(t1)*sin(t2 + t3) -cos(t1); sin(t2 + t3) cos(t2 + t3) 0]
+R4 = [-cos(t1)*sin(t2 + t3 + t4) sin(t1) cos(t1)*cos(t2 + t3 + t4); -sin(t1)*sin(t2 + t3 + t4) -cos(t1) sin(t1)*cos(t2 + t3 + t4); cos(t2 + t3 + t4) 0 sin(t2 + t3 + t4)]
+R5 = [(sin(t1)*sin(t5) - sin(t2 + t3 + t4)*cos(t1)*cos(t5)) (sin(t1)*cos(t5) + sin(t2 + t3 + t4)*cos(t1)*sin(t5)) (cos(t2 + t3 + t4)*cos(t1)); (-cos(t1)*sin(t5) - sin(t2 + t3 + t4)*sin(t1)*cos(t5) ) (-cos(t1)*cos(t5) + sin(t2 + t3 + t4)*sin(t1)*sin(t5)) (cos(t2 + t3 + t4)*sin(t1)); cos(t2 + t3 + t4)*cos(t5) -cos(t2 + t3 + t4)*sin(t5) sin(t2 + t3 + t4)]
