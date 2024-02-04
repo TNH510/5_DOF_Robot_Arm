08bdcacf6f1f2f70d7f4136d8cab1af36dfb6191
Nguyen Ba Vu Thach <thach6656@gmail.com>
2024-02-04 22:28:10 +0700

    update: Create project to use EmguCV to show the image  from received data of camera

---
+++ thayquang/MP05_VeBieuDoHistogram/MP05_VeBieuDoHistogram/MP05_VeBieuDoHistogram/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace MP03_ChuyenAnhMauRGBsangGrayscale
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox_Hinhgoc = new Emgu.CV.UI.ImageBox();
+            this.imageBox_Average = new Emgu.CV.UI.ImageBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.zedGraphHistogram = new ZedGraph.ZedGraphControl();
+            this.Connect_btn = new System.Windows.Forms.Button();
+            this.Image_btn = new System.Windows.Forms.Button();
+            this.Disconnect_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox_Hinhgoc)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox_Average)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox_Hinhgoc
+            // 
+            this.imageBox_Hinhgoc.Location = new System.Drawing.Point(37, 50);
+            this.imageBox_Hinhgoc.Name = "imageBox_Hinhgoc";
+            this.imageBox_Hinhgoc.Size = new System.Drawing.Size(556, 384);
+            this.imageBox_Hinhgoc.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox_Hinhgoc.TabIndex = 2;
+            this.imageBox_Hinhgoc.TabStop = false;
+            // 
+            // imageBox_Average
+            // 
+            this.imageBox_Average.Location = new System.Drawing.Point(37, 521);
+            this.imageBox_Average.Name = "imageBox_Average";
+            this.imageBox_Average.Size = new System.Drawing.Size(556, 372);
+            this.imageBox_Average.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox_Average.TabIndex = 4;
+            this.imageBox_Average.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Hình gốc";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.label2.Location = new System.Drawing.Point(671, 186);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Biểu đồ Histogram";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(34, 488);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Hình mức xám Average";
+            // 
+            // zedGraphHistogram
+            // 
+            this.zedGraphHistogram.Location = new System.Drawing.Point(674, 216);
+            this.zedGraphHistogram.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.zedGraphHistogram.Name = "zedGraphHistogram";
+            this.zedGraphHistogram.ScrollGrace = 0D;
+            this.zedGraphHistogram.ScrollMaxX = 0D;
+            this.zedGraphHistogram.ScrollMaxY = 0D;
+            this.zedGraphHistogram.ScrollMaxY2 = 0D;
+            this.zedGraphHistogram.ScrollMinX = 0D;
+            this.zedGraphHistogram.ScrollMinY = 0D;
+            this.zedGraphHistogram.ScrollMinY2 = 0D;
+            this.zedGraphHistogram.Size = new System.Drawing.Size(919, 610);
+            this.zedGraphHistogram.TabIndex = 10;
+            this.zedGraphHistogram.Load += new System.EventHandler(this.zedGraph_Load);
+            // 
+            // Connect_btn
+            // 
+            this.Connect_btn.Location = new System.Drawing.Point(123, 911);
+            this.Connect_btn.Name = "Connect_btn";
+            this.Connect_btn.Size = new System.Drawing.Size(174, 46);
+            this.Connect_btn.TabIndex = 11;
+            this.Connect_btn.Text = "Connect";
+            this.Connect_btn.UseVisualStyleBackColor = true;
+            this.Connect_btn.Click += new System.EventHandler(this.Connect_btn_Click);
+            // 
+            // Image_btn
+            // 
+            this.Image_btn.Location = new System.Drawing.Point(342, 911);
+            this.Image_btn.Name = "Image_btn";
+            this.Image_btn.Size = new System.Drawing.Size(174, 116);
+            this.Image_btn.TabIndex = 13;
+            this.Image_btn.Text = "Get Image";
+            this.Image_btn.UseVisualStyleBackColor = true;
+            this.Image_btn.Click += new System.EventHandler(this.Image_btn_Click);
+            // 
+            // Disconnect_btn
+            // 
+            this.Disconnect_btn.Location = new System.Drawing.Point(123, 981);
+            this.Disconnect_btn.Name = "Disconnect_btn";
+            this.Disconnect_btn.Size = new System.Drawing.Size(174, 46);
+            this.Disconnect_btn.TabIndex = 14;
+            this.Disconnect_btn.Text = "Disconnect";
+            this.Disconnect_btn.UseVisualStyleBackColor = true;
+            this.Disconnect_btn.Click += new System.EventHandler(this.Disconnect_btn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1638, 1055);
+            this.Controls.Add(this.Disconnect_btn);
+            this.Controls.Add(this.Image_btn);
+            this.Controls.Add(this.Connect_btn);
+            this.Controls.Add(this.zedGraphHistogram);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.imageBox_Average);
+            this.Controls.Add(this.imageBox_Hinhgoc);
+            this.Name = "Form1";
+            this.Text = "Chuyển RGB sang Greyscale";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox_Hinhgoc)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox_Average)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox_Hinhgoc;
+        private Emgu.CV.UI.ImageBox imageBox_Average;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private ZedGraph.ZedGraphControl zedGraphHistogram;
+        private System.Windows.Forms.Button Connect_btn;
+        private System.Windows.Forms.Button Image_btn;
+        private System.Windows.Forms.Button Disconnect_btn;
+    }
+}
+
---
+++ thayquang/MP05_VeBieuDoHistogram/MP05_VeBieuDoHistogram/MP05_VeBieuDoHistogram/Form1.cs
@@ -0,0 +1,344 @@
+﻿//code
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Emgu.Util;
+using ZedGraph;
+using System.Drawing.Imaging;
+
+
+
+namespace MP03_ChuyenAnhMauRGBsangGrayscale
+{
+    public partial class Form1 : Form
+    {
+
+        // Create a socket object
+        Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        string response_client;
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Goi cac ham de ve bieu do histogram
+            //=================================================
+            }
+
+        //Khai bao ham tinh toan muc xam theo phuong phap Average
+        public Image<Bgr, byte> HinhxamAverage(Image<Bgr, byte> hinhgoc)
+        {
+            Image<Bgr, byte> Hinhmucxam = new Image<Bgr, byte>(hinhgoc.Width, hinhgoc.Height);
+
+            for (int x = 0; x < hinhgoc.Width; x++)
+            {
+                for (int y = 0; y < hinhgoc.Height; y++)
+                {
+                    //Lay diem anh
+                    Bgr pixelValue = hinhgoc[y, x];
+                    byte R = (byte)pixelValue.Red;
+                    byte G = (byte)pixelValue.Green;
+                    byte B = (byte)pixelValue.Blue;
+
+                    //Tinh gia tri muc xam
+                    byte gray = (byte)((R + G + B) / 3);
+
+                    //Gan gia tri muc xam vua tinh duoc vao hinh muc xam
+                    Hinhmucxam.Data[y, x, 2] = gray;
+                    Hinhmucxam.Data[y, x, 1] = gray;
+                    Hinhmucxam.Data[y, x, 0] = gray;
+
+                }
+            }
+            return Hinhmucxam;
+        }
+
+        //Tinh histogram cua anh xam
+        public double[] TinhHistogram(Image <Bgr, byte> Hinhmucxam)
+        {
+            //Moi pixel muc xam co gia tri tu 0 den 255, do vay ta khai bao mot mang
+            //co 256 phan tu dung de chua so dem cua cac pixels co cung muc xam trong anh
+            //Dung kieu double vi tong so dem rat lon va phu thuoc vao kich thuoc cua anh
+            double[] histogram = new double[256];
+
+            for (int x = 0; x < Hinhmucxam.Width; x++)
+                for (int y = 0; y < Hinhmucxam.Height; y++)
+                {
+                    Bgr pixelValue = Hinhmucxam[y, x];
+                    byte gray = (byte)pixelValue.Red;
+                    //Lay red vi trong hinh xam red = green = blue
+
+                    //Gia tri gray cung la phan tu thu gray trong mang histogram
+                    //da khai bao. Se tang so dem cua phan tu gray len 1
+                    histogram[gray]++;
+          
+                }
+            return histogram;//So luong diem anh co cung gia tri muc xam
+
+
+        }
+        //Chuyen du lieu tu dang ma tran sang kieu du lieu cua zedgragp
+        PointPairList ChuyendoiHistogram(double[] histogram) 
+        {
+            //PointPairList la kieu du lieu cua Zedgraph de ve bieu do
+            PointPairList points = new PointPairList();
+
+            for (int i = 0; i < histogram.Length; i++)
+            {   
+                //i tuong ung truc nam ngang tu 0 - 255
+                //Histogram tuong ung truc dung, la so pixel cung 
+                points.Add(i, histogram[i]);
+
+                
+            }
+            return points;
+
+        }
+
+        //Thiet lap mot bieu do trong Zedgraph
+        public GraphPane BieudoHistogram(PointPairList histogram)
+        {
+            //GraphPane la doi tuong trong Zedgraph
+            GraphPane gp = new GraphPane();
+
+            gp.Title.Text = @"Biểu đồ Histogram";
+            gp.Rect = new Rectangle(0, 0, 700, 500);//Khung chua bieu do
+
+            //Thiet lap truc ngang
+            gp.XAxis.Title.Text = @"Giá trị mức xám của các điểm ảnh";
+            gp.XAxis.Scale.Min = 0; // nho nhat la 0
+            gp.XAxis.Scale.Max = 255; // lon nhat la 255
+            gp.XAxis.Scale.MajorStep = 5;// Moi buoc chinh la 5
+            gp.XAxis.Scale.MinorStep = 1; //Moi buoc trong mot buoc chinh la 1
+
+            //Tuong tu thiet lap cho truc dung
+            gp.YAxis.Title.Text = @"Số điểm ảnh có cùng mức xám";
+            gp.YAxis.Scale.Min = 0; // nho nhat la 0
+            gp.YAxis.Scale.Max = 15000; // So nay phai lon hon kich thuoc anh
+            gp.YAxis.Scale.MajorStep = 5;// Moi buoc chinh la 5
+            gp.YAxis.Scale.MinorStep = 1; //Moi buoc trong mot buoc chinh la 1
+
+            //Dung bieu do dang bar de bieu dien histgram
+            gp.AddBar("Histogram", histogram, Color.OrangeRed);
+            return gp;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void zedGraph_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Connect_btn_Click(object sender, EventArgs e)
+        {
+            Connect_btn.Enabled = false;
+            Disconnect_btn.Enabled = true;
+            // Connect to the server
+            string host = "192.168.0.49";
+            int port = Convert.ToInt16("2011");
+
+            try
+            {
+                clientSocket.Connect(host, port);
+                Console.WriteLine("Connected");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unable to connect");
+            }
+        }
+
+        private void Image_btn_Click(object sender, EventArgs e)
+        {
+            // Perform your desired action here
+            string filepathtosave = @"C:\Users\daveb\Desktop\raw_data\Image\";
+            // Perform your desired action here
+            string CaptureImageMessage = "1003t\r\n";
+            byte[] CaptureImageBytes = Encoding.ASCII.GetBytes(CaptureImageMessage);
+            clientSocket.Send(CaptureImageBytes);
+            // Receive the response from the server
+            var buffer = new byte[308295];
+            int bytesRead = clientSocket.Receive(buffer);
+
+            System.Threading.Thread.Sleep(1000); // Simulating a 2-second delay
+
+            string RequestImageMessage = "1003I?\r\n";
+            // Send the command to the server
+            byte[] RequestImageBytes = Encoding.ASCII.GetBytes(RequestImageMessage);
+            clientSocket.Send(RequestImageBytes);
+
+            string sentencetosend = "1003I?\r\n";
+
+            // Receive the response from the server
+            buffer = new byte[308295];
+            bytesRead = clientSocket.Receive(buffer);
+
+            if (RequestImageMessage == sentencetosend)
+            {
+                response_client = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                //PrintLog("", "", response_client);
+                //Console.WriteLine(response_client);
+                while (bytesRead < 308291)
+                {
+                    bytesRead += clientSocket.Receive(buffer, bytesRead, 308291 - bytesRead, SocketFlags.None);
+                }
+            }
+
+            response_client = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+            //PrintLog("Data received", "", response_client);
+
+            // Sentences to remove
+            string[] sentencesToRemove = { "1003000308278", "1003*", "1003?", "1003000307200" };
+            //Console.WriteLine("hello");
+            // Loop through each sentence and replace it with an empty string
+            foreach (string sentence in sentencesToRemove)
+            {
+                response_client = response_client.Replace(sentence, "");
+            }
+
+            // Convert the modified response string back to bytes
+            byte[] modifiedBuffer = Encoding.ASCII.GetBytes(response_client);
+            // Convert each byte to its string representation
+            string[] byteStrings = new string[modifiedBuffer.Length];
+            for (int i = 0; i < modifiedBuffer.Length; i++)
+            {
+                byteStrings[i] = modifiedBuffer[i].ToString();
+            }
+            // Join the string representations of bytes
+            string bmpString = string.Join(" ", byteStrings);
+            // Split the byte string into individual byte values
+            string[] byteValues = bmpString.Split();
+
+            // Convert each byte value from string to integer
+            List<byte> byteData = new List<byte>();
+
+
+            foreach (string byteString in byteValues)
+            {
+                byteData.Add(Convert.ToByte(byteString));
+            }
+            
+            // Define the number of bytes to delete from the beginning
+
+            // Bitmap Header(14 bytes) + Bitmap Information (40 bytes) + Color Palette (4 * 256) = 1078 bytes to delete
+
+            // The kinds of image format (RAW or bitmap) is based on the configuration on E2D200.exe
+            int bytesToDelete = 1078; // Adjust this number according to your requirement
+
+            // Delete the specified number of bytes from the beginning
+            byteData.RemoveRange(0, bytesToDelete);
+
+            // Convert the list of bytes back to byte array
+            byte[] byteArrayModified = byteData.ToArray();
+
+            // Determine the dimensions of the original image
+            int width = 640;  // Adjust according to your image width
+            int height = 480;  // Adjust according to your image height
+
+            // Calculate the new dimensions of the image after removing bytes
+            int newWidth = width;  // Since bytes removed from the beginning don't affect width
+            int newHeight = height;  // Adjust height accordingly
+
+            // convert from 1-D array to 2-D array
+            byte[,] byteArray2D = new byte[newHeight, newWidth];
+            byteArray2D = ConvertTo2DArray(byteArrayModified, newHeight, newWidth);
+
+            Image<Bgr, byte> Hinhmucxam = new Image<Bgr, byte>(newWidth, newHeight);
+
+
+            for (int x = 0; x < newWidth; x++)
+            {
+                for (int y = 0; y < newHeight; y++)
+                {
+
+                    //Gan gia tri muc xam vua tinh duoc vao hinh muc xam
+                    Hinhmucxam.Data[y, x, 2] = byteArray2D[y, x];
+                    Hinhmucxam.Data[y, x, 1] = byteArray2D[y, x];
+                    Hinhmucxam.Data[y, x, 0] = byteArray2D[y, x];
+                }
+            }
+            //Hien thi hinh goc trong picBox_Hinhgoc da tao
+
+            imageBox_Hinhgoc.Image = Hinhmucxam;
+
+            //Tinh histogram
+            double[] histogram = TinhHistogram(Hinhmucxam);
+
+            //Chuyen doi kieu du lieu
+            PointPairList points = ChuyendoiHistogram(histogram);
+
+            //Ve bieu do histogram va cho hien thi
+            zedGraphHistogram.GraphPane = BieudoHistogram(points);
+            zedGraphHistogram.Refresh();
+
+        }
+
+        public static byte[,] ConvertTo2DArray(byte[] array1D, int rows, int columns)
+        {
+            byte[,] array2D = new byte[rows, columns];
+            int index = 0;
+
+            // Iterate over the elements of the 1D array and assign them to the 2D array
+            for (int i = rows - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    // Check if there are enough elements in the 1D array
+                    if (index < array1D.Length)
+                    {
+                        array2D[i, j] = array1D[index];
+                        index++;
+                    }
+                    else
+                    {
+                        // If there are not enough elements, you may handle this case according to your requirements
+                        // For example, you can fill remaining elements with default values or throw an exception
+                        // Here, I'm filling remaining elements with 0
+                        array2D[i, j] = 0;
+                    }
+                }
+            }
+            return array2D;
+        }
+
+        private void Disconnect_btn_Click(object sender, EventArgs e)
+        {
+            Connect_btn.Enabled = true;
+            Disconnect_btn.Enabled = false;
+            try
+            {
+                clientSocket.Close();
+                Console.WriteLine("Disconnected");
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Unable to disconnect");
+            }
+        }
+    }
+}
