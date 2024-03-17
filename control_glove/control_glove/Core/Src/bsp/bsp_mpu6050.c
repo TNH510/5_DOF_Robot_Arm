577be5f2b170a0e0d9da3ae9993123fd993f8b32
Hieu Tran Ngoc <hieutranngoc@itrvn.com>
2024-03-17 11:45:39 +0700

    update: Apply madgwickAHRS for IMU success

--- control_glove/control_glove/Core/Src/bsp/bsp_mpu6050.c
+++ control_glove/control_glove/Core/Src/bsp/bsp_mpu6050.c
@@ -442,7 +442,9 @@ References:
 #define MPU6050_GXGAIN                        16.384    // FS_SEL = 3, +/-2000degree/s, MPU6050_GYRO_FS_2000
 #define MPU6050_GYGAIN                        16.384    // FS_SEL = 3, +/-2000degree/s, MPU6050_GYRO_FS_2000
 #define MPU6050_GZGAIN                        16.384    // FS_SEL = 3, +/-2000degree/s, MPU6050_GYRO_FS_2000
+#define betaDef		0.15f		// 2 * proportional gain
 
+static volatile float beta = betaDef;			   // 2 * proportional gain (Kp)
 static volatile float twoKp = twoKpDef;    // 2 * proportional gain (Kp)
 static volatile float twoKi = twoKiDef;    // 2 * integral gain (Ki)
 static volatile float q0 = 1.0f, q1 = 0.0f, q2 = 0.0f,
@@ -470,6 +472,7 @@ static void    mpu6050_GetData(void);
 static void    mpu6050_updateQuaternion(void);
 static void    MahonyAHRSupdateIMU(float gx, float gy, float gz, float ax, float ay, float az);
 static void    mpu6050_getRollPitchYaw(void);
+static float   invSqrt(float x); 
 /*-----------------------------------------------------------------------------------------------*/
 
 base_status_t bsp_mpu6050_filter_task(void)
@@ -509,7 +512,7 @@ base_status_t bsp_mpu6050_filter_task(void)
         
     }
 
-    printf("500,0,%d\r\n", z_pos);
+    printf("%0.2f,%0.2f,%0.2f\r\n", pitch, roll, yaw);
 
 	HAL_Delay(100);
 
@@ -585,71 +588,71 @@ static void mpu6050_updateQuaternion(void)
 
 static void MahonyAHRSupdateIMU(float gx, float gy, float gz, float ax, float ay, float az)
 {
-    float norm;
-    float halfvx, halfvy, halfvz;
-    float halfex, halfey, halfez;
-    float qa, qb, qc;
+	float recipNorm;
+	float s0, s1, s2, s3;
+	float qDot1, qDot2, qDot3, qDot4;
+	float _2q0, _2q1, _2q2, _2q3, _4q0, _4q1, _4q2 ,_8q1, _8q2, q0q0, q1q1, q2q2, q3q3;
 
-    // Compute feedback only if accelerometer measurement valid (avoids NaN in accelerometer normalisation)
-    if (!((ax == 0.0f) && (ay == 0.0f) && (az == 0.0f)))
-    {
-        // Normalise accelerometer measurement
-        norm = sqrt(ax * ax + ay * ay + az * az);
-        ax /= norm;
-        ay /= norm;
-        az /= norm;
+	// Rate of change of quaternion from gyroscope
+	qDot1 = 0.5f * (-q1 * gx - q2 * gy - q3 * gz);
+	qDot2 = 0.5f * (q0 * gx + q2 * gz - q3 * gy);
+	qDot3 = 0.5f * (q0 * gy - q1 * gz + q3 * gx);
+	qDot4 = 0.5f * (q0 * gz + q1 * gy - q2 * gx);
 
-        // Estimated direction of gravity and vector perpendicular to magnetic flux
-        halfvx = q1 * q3 - q0 * q2;
-        halfvy = q0 * q1 + q2 * q3;
-        halfvz = q0 * q0 - 0.5f + q3 * q3;
+	// Compute feedback only if accelerometer measurement valid (avoids NaN in accelerometer normalisation)
+	if(!((ax == 0.0f) && (ay == 0.0f) && (az == 0.0f))) {
 
-        // Error is sum of cross product between estimated and measured direction of gravity
-        halfex = (ay * halfvz - az * halfvy);
-        halfey = (az * halfvx - ax * halfvz);
-        halfez = (ax * halfvy - ay * halfvx);
+		// Normalise accelerometer measurement
+		recipNorm = invSqrt(ax * ax + ay * ay + az * az);
+		ax *= recipNorm;
+		ay *= recipNorm;
+		az *= recipNorm;   
 
-        // Compute and apply integral feedback if enabled
-        if (twoKi > 0.0f)
-        {
-            integralFBx += twoKi * halfex * (1.0f / sampleFreq);    // integral error scaled by Ki
-            integralFBy += twoKi * halfey * (1.0f / sampleFreq);
-            integralFBz += twoKi * halfez * (1.0f / sampleFreq);
-            gx += integralFBx;    // apply integral feedback
-            gy += integralFBy;
-            gz += integralFBz;
-        }
-        else
-        {
-            integralFBx = 0.0f;    // prevent integral windup
-            integralFBy = 0.0f;
-            integralFBz = 0.0f;
-        }
+		// Auxiliary variables to avoid repeated arithmetic
+		_2q0 = 2.0f * q0;
+		_2q1 = 2.0f * q1;
+		_2q2 = 2.0f * q2;
+		_2q3 = 2.0f * q3;
+		_4q0 = 4.0f * q0;
+		_4q1 = 4.0f * q1;
+		_4q2 = 4.0f * q2;
+		_8q1 = 8.0f * q1;
+		_8q2 = 8.0f * q2;
+		q0q0 = q0 * q0;
+		q1q1 = q1 * q1;
+		q2q2 = q2 * q2;
+		q3q3 = q3 * q3;
 
-        // Apply proportional feedback
-        gx += twoKp * halfex;
-        gy += twoKp * halfey;
-        gz += twoKp * halfez;
-    }
+		// Gradient decent algorithm corrective step
+		s0 = _4q0 * q2q2 + _2q2 * ax + _4q0 * q1q1 - _2q1 * ay;
+		s1 = _4q1 * q3q3 - _2q3 * ax + 4.0f * q0q0 * q1 - _2q0 * ay - _4q1 + _8q1 * q1q1 + _8q1 * q2q2 + _4q1 * az;
+		s2 = 4.0f * q0q0 * q2 + _2q0 * ax + _4q2 * q3q3 - _2q3 * ay - _4q2 + _8q2 * q1q1 + _8q2 * q2q2 + _4q2 * az;
+		s3 = 4.0f * q1q1 * q3 - _2q1 * ax + 4.0f * q2q2 * q3 - _2q2 * ay;
+		recipNorm = invSqrt(s0 * s0 + s1 * s1 + s2 * s2 + s3 * s3); // normalise step magnitude
+		s0 *= recipNorm;
+		s1 *= recipNorm;
+		s2 *= recipNorm;
+		s3 *= recipNorm;
 
-    // Integrate rate of change of quaternion
-    gx *= (0.5f * (1.0f / sampleFreq));    // pre-multiply common factors
-    gy *= (0.5f * (1.0f / sampleFreq));
-    gz *= (0.5f * (1.0f / sampleFreq));
-    qa = q0;
-    qb = q1;
-    qc = q2;
-    q0 += (-qb * gx - qc * gy - q3 * gz);
-    q1 += (qa * gx + qc * gz - q3 * gy);
-    q2 += (qa * gy - qb * gz + q3 * gx);
-    q3 += (qa * gz + qb * gy - qc * gx);
+		// Apply feedback step
+		qDot1 -= beta * s0;
+		qDot2 -= beta * s1;
+		qDot3 -= beta * s2;
+		qDot4 -= beta * s3;
+	}
 
-    // Normalise quaternion
-    norm = sqrt(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3);
-    q0 /= norm;
-    q1 /= norm;
-    q2 /= norm;
-    q3 /= norm;
+	// Integrate rate of change of quaternion to yield quaternion
+	q0 += qDot1 * (1.0f / sampleFreq);
+	q1 += qDot2 * (1.0f / sampleFreq);
+	q2 += qDot3 * (1.0f / sampleFreq);
+	q3 += qDot4 * (1.0f / sampleFreq);
+
+	// Normalise quaternion
+	recipNorm = invSqrt(q0 * q0 + q1 * q1 + q2 * q2 + q3 * q3);
+	q0 *= recipNorm;
+	q1 *= recipNorm;
+	q2 *= recipNorm;
+	q3 *= recipNorm;
 }
 
 static void mpu6050_getRollPitchYaw(void)
@@ -676,4 +679,18 @@ static uint8_t readByte(uint8_t address, uint8_t subAddress)
     readBytes(address, subAddress, 1, &dest);
 
     return dest;
+}
+
+// Fast inverse square-root
+// See: http://en.wikipedia.org/wiki/Fast_inverse_square_root
+
+static float invSqrt(float x) 
+{
+	float halfx = 0.5f * x;
+	float y = x;
+	long i = *(long*)&y;
+	i = 0x5f3759df - (i>>1);
+	y = *(float*)&i;
+	y = y * (1.5f - (halfx * y * y));
+	return y;
 }
