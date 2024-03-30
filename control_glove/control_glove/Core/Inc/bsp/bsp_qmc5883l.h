1893f416310f807d53b0ac1842eac8ef9d7af267
Hieu Tran Ngoc <hieutranngoc@itrvn.com>
2024-03-30 22:58:43 +0700

    update: Add QMC5883L APIs

--- control_glove/control_glove/Core/Inc/bsp/bsp_qmc5883l.h
+++ control_glove/control_glove/Core/Inc/bsp/bsp_qmc5883l.h
@@ -4,7 +4,7 @@
  * @version    1.0.0
  * @date       03-2024
  * @author     Hieu Tran Ngoc
- * @brief      Board support package for MARG sensor 
+ * @brief      Board support package for MARG sensor
  * @note       None
  */
 /* Define to prevent recursive inclusion ------------------------------------ */
@@ -12,20 +12,76 @@
 #define __BSP_QMC5883L_H
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 /* Includes ----------------------------------------------------------------- */
-#include <stdint.h>
 #include "bsp_common.h"
+
+#include <stdint.h>
 
 /* Public defines ----------------------------------------------------------- */
 /* Public enumerate/structure ----------------------------------------------- */
+typedef enum STATUS_VARIABLES
+{
+    NORMAL,
+    NO_NEW_DATA,
+    NEW_DATA_IS_READY,
+    DATA_OVERFLOW,
+    DATA_SKIPPED_FOR_READING
+} _qmc5883l_status;
+
+typedef enum MODE_VARIABLES
+{
+    MODE_CONTROL_STANDBY    = 0x00,
+    MODE_CONTROL_CONTINUOUS = 0x01
+} _qmc5883l_MODE;
+
+typedef enum ODR_VARIABLES
+{
+    OUTPUT_DATA_RATE_10HZ  = 0x00,
+    OUTPUT_DATA_RATE_50HZ  = 0x04,
+    OUTPUT_DATA_RATE_100HZ = 0x08,
+    OUTPUT_DATA_RATE_200HZ = 0x0C
+} _qmc5883l_ODR;
+
+typedef enum RNG_VARIABLES
+{
+    FULL_SCALE_2G = 0x00,
+    FULL_SCALE_8G = 0x10
+} _qmc5883l_RNG;
+
+typedef enum OSR_VARIABLES
+{
+    OVER_SAMPLE_RATIO_512 = 0x00,
+    OVER_SAMPLE_RATIO_256 = 0x40,
+    OVER_SAMPLE_RATIO_128 = 0x80,
+    OVER_SAMPLE_RATIO_64  = 0xC0
+} _qmc5883l_OSR;
+
+typedef enum INTTERRUPT_VARIABLES
+{
+    INTERRUPT_DISABLE,
+    INTERRUPT_ENABLE
+} _qmc5883l_INT;
 /* Public macros ------------------------------------------------------------ */
 /* Public variables --------------------------------------------------------- */
 /* Public APIs -------------------------------------------------------------- */
-base_status_t bsp_qmc5883l_init(void);
-
+base_status_t           bsp_qmc5883l_init(void);
+uint8_t          QMC5883L_Read_Reg(uint8_t reg);
+void             QMC5883L_Write_Reg(uint8_t reg, uint8_t data);
+_qmc5883l_status QMC5883L_DataIsReady(void);
+_qmc5883l_status QMC5883L_DataIsOverflow(void);
+_qmc5883l_status QMC5883L_DataIsSkipped(void);
+int16_t          QMC5883L_Read_Temperature(void);
+void             QMC5883L_Read_Data(int16_t *MagX, int16_t *MagY, int16_t *MagZ);
+void  QMC5883L_Initialize(_qmc5883l_MODE MODE, _qmc5883l_ODR ODR, _qmc5883l_RNG RNG, _qmc5883l_OSR OSR);
+void  QMC5883L_Reset(void);
+void  QMC5883L_InterruptConfig(_qmc5883l_INT INT);
+void  QMC5883L_ResetCalibration(void);
+float QMC5883L_Heading(int16_t Xraw, int16_t Yraw, int16_t Zraw);
+void  QMC5883L_Scale(int16_t *X, int16_t *Y, int16_t *Z);
 /* -------------------------------------------------------------------------- */
 #ifdef __cplusplus
 } /* extern "C" { */
--- control_glove/control_glove/Core/Src/bsp/bsp_qmc5883l.c
+++ control_glove/control_glove/Core/Src/bsp/bsp_qmc5883l.c
@@ -9,18 +9,170 @@
  */
 /* Public includes ---------------------------------------------------------- */
 #include "bsp_qmc5883l.h"
+#include "stm32f4xx_hal.h"
+#include "math.h"
 
 /* Private includes --------------------------------------------------------- */
+extern I2C_HandleTypeDef hi2c1;
 /* Private defines ---------------------------------------------------------- */
+#define QMC5883L_ADDRESS  		0x1A
+#define QMC5883L_I2C_PORT		&hi2c1
+
+#define QMC5883L_DATA_READ_X_LSB	0x00
+#define QMC5883L_DATA_READ_X_MSB	0x01
+#define QMC5883L_DATA_READ_Y_LSB	0x02
+#define QMC5883L_DATA_READ_Y_MSB	0x03
+#define QMC5883L_DATA_READ_Z_LSB	0x04
+#define QMC5883L_DATA_READ_Z_MSB	0x05
+#define QMC5883L_TEMP_READ_LSB		0x07
+#define QMC5883L_TEMP_READ_MSB		0x08 
+#define QMC5883L_STATUS		        0x06 // DOR | OVL | DRDY
+#define QMC5883L_CONFIG_1		0x09 // OSR | RNG | ODR | MODE
+#define QMC5883L_CONFIG_2		0x0A // SOFT_RST | ROL_PNT | INT_ENB
+#define QMC5883L_CONFIG_3		0x0B // SET/RESET Period FBR [7:0]
+#define QMC5883L_ID			0x0D
+
+#ifndef M_PI 
+#define M_PI 3.14159265358979323846264338327950288f 
+#endif 
+
+#define QMC5883L_SCALE_FACTOR 		0.732421875f
+#define QMC5883L_CONVERT_GAUSS_2G 	12000.0f
+#define QMC5883L_CONVERT_GAUSS_8G 	3000.0f
+#define QMC5883L_CONVERT_MICROTESLA 	100
+#define QMC5883L_DECLINATION_ANGLE	93.67/1000  // radian, Tekirdag/Turkey
+
 /* Private enumerate/structure ---------------------------------------------- */
 /* Private macros ----------------------------------------------------------- */
 /* Public variables --------------------------------------------------------- */
 /* Private variables -------------------------------------------------------- */
+static float Xmin,Xmax,Ymin,Ymax;
 /* Private prototypes ------------------------------------------------------- */
 /* Public implementations --------------------------------------------------- */
 base_status_t bsp_qmc5883l_init(void)
 {
+    QMC5883L_Initialize(MODE_CONTROL_CONTINUOUS, OUTPUT_DATA_RATE_100HZ, FULL_SCALE_8G, OVER_SAMPLE_RATIO_256);
     return BS_OK;
+}
+
+uint8_t QMC5883L_Read_Reg(uint8_t reg)
+{
+	uint8_t Buffer[1];
+	HAL_I2C_Mem_Read(QMC5883L_I2C_PORT,QMC5883L_ADDRESS,reg,1,Buffer,1,10);
+	return Buffer[0];
+}
+
+void QMC5883L_Write_Reg(uint8_t reg, uint8_t data)
+{
+	uint8_t Buffer[2]={reg,data};
+	HAL_I2C_Master_Transmit(QMC5883L_I2C_PORT,QMC5883L_ADDRESS,Buffer,2,10);
+}
+
+
+void QMC5883L_Read_Data(int16_t *MagX,int16_t *MagY,int16_t *MagZ) // (-32768 / +32768)
+{
+	*MagX=((int16_t)QMC5883L_Read_Reg(QMC5883L_DATA_READ_X_LSB) | (((int16_t)QMC5883L_Read_Reg(QMC5883L_DATA_READ_X_MSB))<<8));
+	*MagY=((int16_t)QMC5883L_Read_Reg(QMC5883L_DATA_READ_Y_LSB) | (((int16_t)QMC5883L_Read_Reg(QMC5883L_DATA_READ_Y_MSB))<<8));
+	*MagZ=((int16_t)QMC5883L_Read_Reg(QMC5883L_DATA_READ_Z_LSB) | (((int16_t)QMC5883L_Read_Reg(QMC5883L_DATA_READ_Z_MSB))<<8));
+}
+
+
+int16_t QMC5883L_Read_Temperature()
+{
+	return (((int16_t)QMC5883L_Read_Reg(QMC5883L_TEMP_READ_LSB)) | (((int16_t)QMC5883L_Read_Reg(QMC5883L_TEMP_READ_MSB))<<8))/100;
+}
+
+
+void QMC5883L_Initialize(_qmc5883l_MODE MODE,_qmc5883l_ODR ODR,_qmc5883l_RNG RNG,_qmc5883l_OSR OSR)
+{
+	QMC5883L_Write_Reg(QMC5883L_CONFIG_3,0x01);
+	QMC5883L_Write_Reg(QMC5883L_CONFIG_1,MODE | ODR | RNG | OSR);
+}
+
+void QMC5883L_Reset()
+{
+	QMC5883L_Write_Reg(QMC5883L_CONFIG_2,0x81);
+}
+
+void QMC5883L_InterruptConfig(_qmc5883l_INT INT)
+{
+	if(INT==INTERRUPT_ENABLE){QMC5883L_Write_Reg(QMC5883L_CONFIG_2,0x00);}
+	else {QMC5883L_Write_Reg(QMC5883L_CONFIG_2,0x01);}
+}
+
+
+_qmc5883l_status QMC5883L_DataIsReady()
+{
+	uint8_t Buffer=QMC5883L_Read_Reg(QMC5883L_STATUS);
+	if((Buffer&0x00)==0x00)	  {return NO_NEW_DATA;}
+	else if((Buffer&0x01)==0X01){return NEW_DATA_IS_READY;}
+	return NORMAL;
+}
+
+_qmc5883l_status QMC5883L_DataIsSkipped()
+{
+	uint8_t Buffer=QMC5883L_Read_Reg(QMC5883L_STATUS);
+	if((Buffer&0x00)==0X00)	  {return NORMAL;}
+	else if((Buffer&0x04)==0X04){return DATA_SKIPPED_FOR_READING;}
+		return NORMAL;
+}
+
+_qmc5883l_status QMC5883L_DataIsOverflow()
+{
+	uint8_t Buffer=QMC5883L_Read_Reg(QMC5883L_STATUS);
+	if((Buffer&0x00)==0X00)	  {return NORMAL;}
+	else if((Buffer&0x02)==0X02){return DATA_OVERFLOW;}
+		return NORMAL;
+}
+
+
+void QMC5883L_ResetCalibration() 
+{ 
+	Xmin=Xmax=Ymin=Ymax=0;
+} 
+
+ 
+float QMC5883L_Heading(int16_t Xraw,int16_t Yraw,int16_t Zraw) 
+{ 
+   	float X=Xraw,Y=Yraw;
+   	float Heading;
+
+  	if(X<Xmin) {Xmin = X;} 
+    	  else if(X>Xmax) {Xmax = X;} 
+
+  	if(Y<Ymin) {Ymin = Y;} 
+    	  else if(Y>Ymax) {Ymax = Y;} 
+
+  
+  	if( Xmin==Xmax || Ymin==Ymax ) {return 0.0;} 
+
+ 
+ 	  X -= (Xmax+Xmin)/2; 
+  	Y -= (Ymax+Ymin)/2; 
+    
+  	X = X/(Xmax-Xmin); 
+  	Y = Y/(Ymax-Ymin); 
+
+  	Heading = atan2(Y,X); 
+		//EAST
+	Heading += QMC5883L_DECLINATION_ANGLE;
+	//WEST
+	//Heading -= QMC5883L_DECLINATION_ANGLE;
+		
+	if(Heading <0)
+   	  {Heading += 2*M_PI;}
+	else if(Heading > 2*M_PI)
+   	  {Heading -= 2*M_PI;}
+ 
+ return Heading; 
+} 
+
+
+void QMC5883L_Scale(int16_t *X,int16_t *Y,int16_t *Z)
+{
+	*X*=QMC5883L_SCALE_FACTOR;
+	*Y*=QMC5883L_SCALE_FACTOR;
+	*Z*=QMC5883L_SCALE_FACTOR;
 }
 
 /* Private implementations -------------------------------------------------- */
